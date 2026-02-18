911fffa082e1265ff19b9b864d2e129544693c85
Henrique Goncalves <henriquegoncalvesdev@gmail.com>
2026-02-18 09:17:26 -0300

    feat: Add a concurrent development script and configure Turbopack root in Next.js.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Manual root configuration for Turbopack
+  turbopack: {
+    root: "/Users/henriquegoncalves/Documents/Projects/rubber-duck-debugger",
+  },
 };
 
 export default nextConfig;
